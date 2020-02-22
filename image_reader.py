81a9d71814285c42e7775ee31ddadae8bbb3b193
Amirhossein <amirbehzadfar.h@gmail.com>
2020-02-22 11:46:52 +0330

    Image save in GrayScale

--- image_reader.py
+++ image_reader.py
@@ -1,6 +1,12 @@
 import cv2 as cv
 
 path = input('Insert image path\n')
+cv.namedWindow('image', cv.WINDOW_AUTOSIZE)
 img = cv.imread(path, cv.IMREAD_COLOR)
-cv.imshow('img', img)
-cv.waitKey(0)
+cv.imshow('image', img)
+k = cv.waitKey(0)
+
+if ord('s'):
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imwrite('gray_opencv.png', gray_img)
+cv.destroyAllWindows()
